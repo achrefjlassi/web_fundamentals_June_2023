ba93d33134cd00ac5b3df84a33946cab56fc5896
John Doe <johndoe@example.com>
2023-06-22 12:07:47 +0100

    added recreate website Part I

---
+++ W01D04/02_recreate_website/script.js
@@ -0,0 +1,22 @@
+// var x = 5;
+    
+// function setX(newValue) {
+//     console.log();
+// }
+    
+// console.log(x);
+// setX(15);
+// console.log(x+ 100);
+
+var x = 5
+    
+function addToX(amount) {
+    return null
+    console.log( x + amount)
+    // console.log("hello there");
+}
+    
+console.log(x)
+var result = addToX(-10)
+console.log(result)
+console.log(x)
