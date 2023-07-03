ad5c74a81ca3dce0a5ad253639c976f9bc8e23da
John Doe <johndoe@example.com>
2023-07-03 15:44:20 +0100

    Belt Demo

---
+++ W03D01-BELT-DEMO/script.js
@@ -0,0 +1,19 @@
+console.log("Hello from JS");
+function removeButton(e){
+    e.remove()
+}
+
+function voteUp(id){
+    document.getElementById(id).innerText++
+}
+
+function voteDown(id) {
+    document.querySelector(`#${id}`).innerText--
+}
+
+function find() {
+    inputValue = document.getElementById("search").value
+    console.log(inputValue);
+    alert(`Searching for "${inputValue}"`)
+    document.getElementById("search").value=""
+}
